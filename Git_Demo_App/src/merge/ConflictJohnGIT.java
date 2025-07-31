7eaf8d98f7f4ab5e45ef5643162b32bcb395a7d1
Amirul Afiq Shahdan <amirulafiq.shahdan@gmail.com>
2025-07-31 09:51:44 +0800

    1st Commit
    DevOps training

---
+++ Git_Demo_App/src/merge/ConflictJohnGIT.java
@@ -0,0 +1,12 @@
+package merge;
+
+//TODO: DevOps - Refer to DevOps Manual B - Section 1 - Merge from branch to main with conflict 
+
+public class ConflictJohnGIT {
+
+	public String getConflict() {
+
+		return "main";
+	}
+
+}
---
+++ Git_Demo_App/src/merge/SameBranchConflict.java
@@ -0,0 +1,17 @@
+package merge;
+
+//TODO: DevOps - Refer to DevOps Manual B - Section 2 - Merge from branch to branch with conflict 
+
+public class SameBranchConflict {
+
+	public String getMessage() {
+
+		return "Main should be remain unchanged";
+
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
